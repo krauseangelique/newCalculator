8ef10c803b809417630cc47ff4e79af51443cb1c
angelique KRAUSE <krause_angelique@hotmail.com>
2022-07-27 12:17:53 +0200

    javascript functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,43 @@
 // Operations and numbers
+// screen
+const screen = document.querySelector('.screen');
+console.log(screen);
+// recup ddata
+const buttons = Array.from(document.getElementsByTagName('button'));
+console.log(buttons);
+// stock array html collection
+// foreach => map
+buttons.map(button => {
+    console.log(button);
+    // pour chaque boutton je dois l'afficher sur mon screen
+    button.addEventListener('click', (event) => {
+
+        // to screen all the data
+        switch (event.target.innerText) {
+            case 'AC':
+                screen.innerText = "";
+                break;
+
+            case '=':
+                try {
+                    // eval évalue du js
+                    screen.innerText = eval(screen.innerText);
+                } catch {
+                    screen.innerText = "Error";
+                }
+                break;
+
+            case '←':
+                if (screen.innerText) {screen.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+        
+            default:
+                screen.innerText += event.target.innerText;
+                break;
+        }
+
+    });
+});
+    
+
